d230d752c116b05f52983c174f11272c65a2c40e
leahning <ningleah@gmail.com>
2025-07-15 15:29:19 -0700

    added SNAP API endpoint and data loading w pandas

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,16 @@
-import json
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+import json
+import pandas as pd
+import os
+
+
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+SNAP_DATA_PATH = os.path.join('data', 'snap_cleaned.csv')
+snap_df = pd.read_csv(SNAP_DATA_PATH)
 
 @app.route('/api/test')
 def test():
@@ -11,9 +18,19 @@ def test():
 
 @app.route('/api/snap')
 def get_snap_locations():
-    with open('snap_data.json') as f:
-        data = json.load(f)
-    return jsonify(data)
+    data = snap_df[['Store Name', 'Street Number', 'Street Name', 'City', 'State', 'Zip Code', 'Latitude', 'Longitude']]
+
+    results = [] # format each row into a dict
+    for _, row in data.iterrows():
+        full_addr = f"{row['Street Number']} {row['Street Name']}, {row['City']}, {row['State']} {row['Zip Code']}"
+        results.append({
+            'name': row['Store Name'].strip(),
+            'address': full_addr.strip(),
+            'lat': row['Latitude'],
+            'lon': row['Longitude']
+        })
+
+    return jsonify(results)
 
 
 if __name__ == '__main__':
